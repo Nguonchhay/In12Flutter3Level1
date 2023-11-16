5c9bcb046d5c98b401e3962e017b978f62bdfbe2
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2023-11-16 19:55:28 +0700

    [DART] Week 2 task 2

---
+++ dart_demo/week2/task2.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("Week 2 Task 2");
+  var width = 8.0;
+  double height = 2.0;
+
+  double area = width * height;
+  double perimeter = 2 * (width + height);
+
+  print("Area = $area , Perimeter = $perimeter");
+}
