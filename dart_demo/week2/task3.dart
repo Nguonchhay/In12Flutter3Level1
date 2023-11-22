7118733874a4ffa497de0b16246b626074b7b406
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2023-11-22 18:31:02 +0700

    [TASK] Week2 task 3

---
+++ dart_demo/week2/task3.dart
@@ -0,0 +1,13 @@
+void main() {
+  int value1 = 25;
+  int value2 = 50;
+  // if (value1 == 30 || value2 == 30 || value1 + value2 == 30) {
+  //   print("True");
+  // } else {
+  //   print("False");
+  // }
+
+  print(
+    (value1 == 30 || value2 == 30 || value1 + value2 == 30) ? "True" : "False"
+  );
+}
