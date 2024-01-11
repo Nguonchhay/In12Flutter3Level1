0a78f3e5d427c6182ca03b9785f5e6cfef5e62df
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-01-11 19:30:18 +0700

    [TASK] Product screen with filter

--- flutter_ecommerce/lib/screens/products_screen.dart
+++ flutter_ecommerce/lib/screens/products_screen.dart
@@ -13,7 +13,7 @@ class ProductsScreen extends StatefulWidget {
 
 class _ProductsScreenState extends State<ProductsScreen> {
 
-  final latestProducts = [
+  final products = [
     Product(
       name: 'Product 001', 
       imageUrl: 'https://fastly.picsum.photos/id/94/200/200.jpg?hmac=6A2OfXxXR04lSduBcURN4k0_3aiDGcJQjMpSwDmQevg', 
@@ -50,14 +50,99 @@ class _ProductsScreenState extends State<ProductsScreen> {
       id: '005'
     ),
   ];
+  List<Product> _filteredProducts = [];
+  bool _isSearch = false;
+  TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _filteredProducts = products;
+  }
+
+  void _performSearch() {
+    setState(() {
+      _filteredProducts = products.where(
+        (product) => product.name.toLowerCase().contains(_searchController.text.toLowerCase())
+      ).toList();
+    });
+  }
+
+  Widget _buildSearch() {
+    if (!_isSearch) {
+      return const Text('Products');
+    }
+    return TextField(
+        controller: _searchController,
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(),
+        ),
+        onSubmitted: (value) {
+          setState(() {
+            _isSearch = false;
+          });
+        },
+        onChanged: (value) {
+          _performSearch();
+        },
+      );
+  }
+
+  Widget _buildListItem(Product product) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Column(
+        children: [
+          ClipRRect(
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.fill,
+              width: double.infinity,
+            ),
+          ),
+          ListTile(
+            title: Text(product.name),
+            subtitle: Text(product.description),
+            trailing: Text("\$ ${product.unitPrice}"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Products'),
+        title: _buildSearch(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _isSearch = true;
+              });
+            }, 
+            icon: const Icon(Icons.search),
+          ),
+        ],
+        
       ),
-      body: const Placeholder()
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          height: MediaQuery.of(context).size.height,
+          child: _filteredProducts.isEmpty ? const Center(child: Text('No product founds')) : ListView.builder(
+            itemCount: _filteredProducts.length,
+            itemBuilder: (ctx, ind) => _buildListItem(_filteredProducts[ind]),
+          ),
+        ),
+      ),
     );
   }
 }
